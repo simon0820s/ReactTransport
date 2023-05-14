5e7ad6f97d7f3141b1ebf36108e8146ba14e86f6
simon0820s <simon0820s@gmail.com>
2023-05-13 21:41:33 -0500

    upgrade nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
-import { BiAnalyse } from "react-icons/bi";
+import React, { useState } from 'react'
+import Button from './Button';
+import { HiLocationMarker } from "react-icons/hi";
 
 const Nav = () => {
-  const links=[
-    {name:"Home",link:"/"},
-    {name:"Home",link:"/"},
-    {name:"Home",link:"/"},
-    {name:"Home",link:"/"}
-  ]
+    let Links =[
+      {name:"HOME",link:"/"},
+      {name:"SERVICE",link:"/"},
+      {name:"ABOUT",link:"/"},
+      {name:"BLOG'S",link:"/"},
+      {name:"CONTACT",link:"/"},
+    ];
+    let [open,setOpen]=useState(false);
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex bg-white py-4 items-center justify-between'>
-        <div className='flex flex-row font-bold text-2xl ml-3 gap-1/2 cursor-pointer items-center text-gray-800'>
+      <div className='md:flex items-center justify-between bg-neutral-100 py-4 md:px-5 px-10'>
+      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800'>
+        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
+        <HiLocationMarker className='text-2xl pb-1' name="logo-ionic"></HiLocationMarker>
+        </span>
+        Designer
+      </div>
+      
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <ion-icon name={open ? 'close':'menu'}></ion-icon>
+      </div>
 
-          <span className='text-3xl text-green-700 mr-1 pt-1/2'>
-            <BiAnalyse/>
-          </span>
-          Transport
-
-        </div>
-        <ul className='md:flex md:items-center mr-10'>
-          {
-            links.map((link)=>(
-              <li key={link.name} className='md:ml-8 text-xl'>
-                <a href={link.link} className='text-gray-800 hover:text-green-700 transition-all ease-in-out duration-300'>{link.name}</a>
-              </li>
-            ))
-          }
-        </ul>
-
+      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-neutral-100 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+        {
+          Links.map((link)=>(
+            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+              <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+            </li>
+          ))
+        }
+        <Button>
+          Get Started
+        </Button>
+      </ul>
       </div>
     </div>
   )
