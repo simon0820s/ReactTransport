7007a024d0a7b3965243fe71135e35375aca6f59
simon0820s <simon0820s@gmail.com>
2023-05-15 17:01:38 -0500

    re ubication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Nav from "./components/Nav/Nav"
-import Form from "./components/Form/Form"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Nav from "./components/Nav"
 
 const App=()=>{
   return (
     <>
     <div className='bg-gray-800 min-h-screen'>
       <Nav/>
-      <Form/>
     </div>
     </>
   )
