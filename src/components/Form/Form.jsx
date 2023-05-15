cd0cf3375e708a0c43b6db4bfff2f65191f901f1
simon0820s <simon0820s@gmail.com>
2023-05-15 10:33:01 -0500

    add validators, style form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,29 +10,32 @@ const Form = () => {
   }
 
   return (
-    <div className='h-screen w-full flex md:mt-16 mt-14 text-neutral-300'>
+    <div className='flex items-start justify-center h-screen w-full mt-14 text-gray-900'>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form className='flex flex-col md:mt-24 mt-20 p-10 gap-4 bg-gray-600 rounded-3xl' onSubmit={handleSubmit(onSubmit)}>
 
-        <div>
-          <label>Name</label>
-          <input type="text" {...register('name', {required:true})}/>
-          {errors.name?.type==='required' && <p>name is required</p>}
+        <div className='flex flex-col'>
+          <label className='text-indigo-400'>Name</label>
+          <input className='border-none bg-neutral-100 rounded-sm mt-2' type="text" {...register('name', {required:true})}/>
+          {errors.name?.type==='required' && <p className='text-red-500'>Name is required</p>}
+          
         </div>
 
-        <div>
-          <label>Email</label>
-          <input type="text" {...register('email', {pattern:/^[^\s@]+@[^\s@]+\.[^\s@]+$/i})}/>
-          {errors.email?.type==='pattern' && <p>email format is wrong</p>}
+        <div className='flex flex-col'>
+          <label className='text-indigo-400'>Email</label>
+          <input className='border-none bg-neutral-100 rounded-sm mt-2' type="text" {...register('email', {pattern:/^[^\s@]+@[^\s@]+\.[^\s@]+$/i})}/>
+          {errors.email?.type==='pattern' && <p className='text-red-500'>Email format is wrong</p>}
+          {errors.email?.type==='required' && <p className='text-red-500'>Email is required</p>}
         </div>
 
-        <div>
-          <label>Age</label>
-          <input type="text" {...register('age', {required:true})}/>
+        <div className='flex flex-col'>
+          <label className='text-indigo-400'>Age</label>
+          <input className='border-none bg-neutral-100 rounded-sm mt-2' type="text" {...register('age', {required:true})}/>
+          {errors.age?.type==='required' && <p className='text-red-500'>Age is required</p>}
         </div>
 
-        <div>
-          <label>Country</label>
+        <div className='flex flex-col'>
+          <label className='text-indigo-400'>Country</label>
           <select {...register('country', {required:true})}>
             <option value="col">Colombia</option>
             <option value="mex">Mexico</option>
