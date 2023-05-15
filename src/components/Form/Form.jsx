6b98d5cb5c36627cf94759e462b867d2037a3e0f
simon0820s <simon0820s@gmail.com>
2023-05-14 21:51:55 -0500

    add form/hook

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,48 @@
 import React from 'react'
-import Item from './Item'
+import { useForm } from 'react-hook-form'
 
 const Form = () => {
+
+  const {register,formState:{errors},handleSubmit} = useForm();
+
+  const onSubmit=(data)=>{
+    console.log(data);
+  }
+
   return (
-    <div className='relative bg-gray-900 w-full mt-14 h-screen flex items-center justify-center'>
-      <div className='absolute top-12 flex flex-col items-center justify-center bg-gray-700 w-96 gap-4 rounded-2xl'>
-        <Item text="email" type="password" placeHolder="Enter your email"/>
-        <Item text="email" type="text" placeHolder="Enter your email"/>
-        <Item text="email" type="text" placeHolder="Enter your email"/>
-        <Item text="email" type="text" placeHolder="Enter your email"/>
-      </div>
+    <div className='h-screen w-full flex md:mt-16 mt-14 text-neutral-300'>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <div>
+          <label>Name</label>
+          <input type="text" {...register('name', {required:true})}/>
+          {errors.name?.type==='required' && <p>name is required</p>}
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input type="text" {...register('email', {pattern:/^[^\s@]+@[^\s@]+\.[^\s@]+$/i})}/>
+          {errors.email?.type==='pattern' && <p>email format is wrong</p>}
+        </div>
+
+        <div>
+          <label>Age</label>
+          <input type="text" {...register('age', {required:true})}/>
+        </div>
+
+        <div>
+          <label>Country</label>
+          <select {...register('country', {required:true})}>
+            <option value="col">Colombia</option>
+            <option value="mex">Mexico</option>
+            <option value="rus">Rusia</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Send"/>
+
+      </form>
     </div>
   )
 }
