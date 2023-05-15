a6eb770518a0076f1e357a719668a670a0e21e6f
simon0820s <simon0820s@gmail.com>
2023-05-15 11:10:53 -0500

    style form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,38 +12,39 @@ const Form = () => {
   return (
     <div className='flex items-start justify-center h-screen w-full mt-14 text-gray-900'>
 
-      <form className='flex flex-col md:mt-24 mt-20 p-10 gap-4 bg-gray-600 rounded-3xl' onSubmit={handleSubmit(onSubmit)}>
+      <form className='flex flex-col md:mt-24 mt-20 p-10 gap-4 bg-gray-700 rounded-3xl' onSubmit={handleSubmit(onSubmit)}>
 
         <div className='flex flex-col'>
-          <label className='text-indigo-400'>Name</label>
-          <input className='border-none bg-neutral-100 rounded-sm mt-2' type="text" {...register('name', {required:true})}/>
+          <label className='text-indigo-400'>- Name</label>
+          <input className='border-none bg-gray-500 rounded-sm mt-2 focus:outline-none px-1' type="text" {...register('name', {required:true})}/>
           {errors.name?.type==='required' && <p className='text-red-500'>Name is required</p>}
           
         </div>
 
         <div className='flex flex-col'>
-          <label className='text-indigo-400'>Email</label>
-          <input className='border-none bg-neutral-100 rounded-sm mt-2' type="text" {...register('email', {pattern:/^[^\s@]+@[^\s@]+\.[^\s@]+$/i})}/>
+          <label className='text-indigo-400'>- Email</label>
+          <input className='border-none bg-gray-500 rounded-sm mt-2 focus:outline-none px-1' type="text" {...register('email', {pattern:/^[^\s@]+@[^\s@]+\.[^\s@]+$/i})}/>
           {errors.email?.type==='pattern' && <p className='text-red-500'>Email format is wrong</p>}
           {errors.email?.type==='required' && <p className='text-red-500'>Email is required</p>}
         </div>
 
         <div className='flex flex-col'>
-          <label className='text-indigo-400'>Age</label>
-          <input className='border-none bg-neutral-100 rounded-sm mt-2' type="text" {...register('age', {required:true})}/>
+          <label className='text-indigo-400'>- Age</label>
+          <input className='border-none bg-gray-500 rounded-sm mt-2 focus:outline-none px-1' type="text" {...register('age', {required:true})}/>
           {errors.age?.type==='required' && <p className='text-red-500'>Age is required</p>}
         </div>
 
         <div className='flex flex-col'>
-          <label className='text-indigo-400'>Country</label>
-          <select {...register('country', {required:true})}>
-            <option value="col">Colombia</option>
-            <option value="mex">Mexico</option>
-            <option value="rus">Rusia</option>
+          <label className='text-indigo-400'>- Country</label>
+          <select className='w-24 mt-2 bg-gray-500 p-1 rounded-lg' {...register('country', {required:true})}>
+            <option className='rounded-lg' value="col">Colombia</option>
+            <option className='rounded-lg' value="mex">Mexico</option>
+            <option className='rounded-lg' value="rus">Rusia</option>
           </select>
         </div>
 
-        <input type="submit" value="Send"/>
+        <input className='bg-indigo-600 text-neutral-100 py-2 px-6 w-28 m-auto mt-6 rounded hover:bg-indigo-400 
+    duration-500' type="submit" value="Send"/>
 
       </form>
     </div>
