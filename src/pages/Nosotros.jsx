6b883932b99ffe9a44422ce20f1881762cef44fe
simon0820s <simon0820s@gmail.com>
2023-06-11 22:20:36 -0500

    nosotros 360px ready

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -6,29 +6,31 @@ export default function Nosotros() {
       id="nosotros"
       className="h-screen text-gray-800 bg-neutral-100 bg-opacity-80"
     >
-      <div className="flex flex-col items-center text-center justify-between py-20 px-4 gap-10">
+      <div className="flex flex-col items-center text-center justify-between py-20 px-4 sm:px-20 gap-10">
         <div>
-          <h1 className="text-4xl font-bold text-[#2f4a69]">Article</h1>
-          <p className="mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          </p>
-
-          <p className="mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          </p>
-
-          <p className="mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          </p>
-        </div>
-
-        <div>
-          <h1 className="text-4xl text-[#2f4a69]">Information</h1>
-          <p className="mt-4">
+          <h1 className="text-4xl sm:text-5xl font-bold text-[#2f4a69]">
+            Article
+          </h1>
+          <p className="mt-4 sm:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
             reprehenderit corporis nostrum tempora rem quam. A voluptas aut
             quibusdam, ipsa temporibus doloribus, magnam rem vitae perspiciatis,
             tenetur sit asperiores. Vero!
+          </p>
+        </div>
+
+        <div>
+          <h1 className="text-4xl sm:text-5xl text-[#2f4a69]">Information</h1>
+          <p className="mt-4 sm:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+          </p>
+
+          <p className="mt-4 sm:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+          </p>
+
+          <p className="mt-4 sm:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
           </p>
         </div>
       </div>
